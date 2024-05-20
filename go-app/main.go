7ec4354e2888640f9d11a0a2d4cbeff6c2c77d78
shraiyap <shraiya31parashar@gmail.com>
2024-05-20 15:46:43 +0530

    initial commit for GO

---
+++ go-app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from GO!")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorldHandler)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
